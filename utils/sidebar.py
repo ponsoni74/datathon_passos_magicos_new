a27ff4b8f2f0415977affbc5f942ea8a0e647330
ponsoni74 <vponsoni@yahoo.com.br>
2025-02-14 13:24:04 -0300

    Update sidebar.py

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -5,11 +5,11 @@ def show_sidebar():
     # Sidebar definitions
     with st.sidebar:
 
-        st.markdown('''Valdinei Ponsoni Linartevich | RM355996<br>
+        st.markdown('''<b><font color='orange'>Aluno</font></b> <br> Valdinei Ponsoni Linartevich | RM355996<br>
                     <b><font color='orange'>Curso</font></b> <br> Pós-Tech Data Analytics | 5DTAT |  FIAP + Alura 
                     <br> <br><b><font color='orange'>Link GitHub</font></b> <br> https://github.com/ponsoni74/passos-magicos-datathon-fiap
                     <br><b><font color='orange'>Link Streamlit</font></b> <br> https://appdatathonpaappsmagicos-9tufmntmbmijerth7aev7k.streamlit.app/
-                     <br> <br>  <br>  <br>  <b><font color='orange'>Aluno</font></b> <br>
+                     <br> <br>  <br>  <br>  
 
             
                     ''', unsafe_allow_html=True)
