1d3178055c6d84c89f221a04ce355486e56aa115
ponsoni74 <vponsoni@yahoo.com.br>
2025-02-19 06:13:54 -0300

    Update sidebar.py
    A atualização foi realizada apenas para inserção do link correto do app na plataforma do Streamlit

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -8,7 +8,7 @@ def show_sidebar():
         st.markdown('''<b><font color='orange'>Aluno</font></b> <br> Valdinei Ponsoni Linartevich | RM355996<br>
                     <b><font color='orange'>Curso</font></b> <br> Pós-Tech Data Analytics | 5DTAT |  FIAP + Alura 
                     <br> <br><b><font color='orange'>Link GitHub</font></b> <br> https://github.com/ponsoni74/passos-magicos-datathon-fiap
-                    <br><b><font color='orange'>Link Streamlit</font></b> <br> https://appdatathonpaappsmagicos-9tufmntmbmijerth7aev7k.streamlit.app/
+                    <br><b><font color='orange'>Link Streamlit</font></b> <br> https://datathonpaappsmagicosnew-5o5qbiyd5rfbhnjkgfhudr.streamlit.app/
                      <br> <br>  <br>  <br>  
 
             
